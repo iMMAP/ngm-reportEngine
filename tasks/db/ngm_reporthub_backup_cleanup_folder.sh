be2e69dc91da5ac9dd10fd8ac4483f3d369f12ea
Tim <thedrfaustus@gmail.com>
2021-03-30 18:21:01 +0430

    update(backups): add # of days argument

--- tasks/db/ngm_reporthub_backup_cleanup_folder.sh
+++ tasks/db/ngm_reporthub_backup_cleanup_folder.sh
@@ -5,8 +5,20 @@
 #   - remove files and folders more than number of DAYS
 #
 ##################################################
+
 DIR=/home/ubuntu/data/reportHub/
 
-DAYS=100
+
+if [ "$#" -eq  "0" ]
+then
+    DAYS=100
+else
+    DAYS=$1
+fi
+
+echo "RUNNING OVER $DAYS DAYS FOLDER CLEANUP"
 
 sudo find $DIR -mindepth 1 -mtime +$DAYS -delete
+
+echo "DONE"
+
