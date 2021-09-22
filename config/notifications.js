7435aaed9fa2390311376228effe03987ce2f083
fakhrihawari <blitzfakhri@gmail.com>
2021-09-22 16:50:36 +0700

    update: setReportsReminderBiweekly

--- config/notifications.js
+++ config/notifications.js
@@ -29,3 +29,37 @@ const REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG = [
 ]
 
 module.exports.REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG = REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG;
+
+const BIWEEKLY_REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG = [
+  {
+    admin0pcode: "AF",
+    first:{
+      soon: [8],
+      pending: [15],
+      today: [10],
+      reporting_due_date: 10
+    },
+    second: {
+      soon: [23],
+      pending: [22],
+      today: [27],
+      reporting_due_date: 27
+    }
+  },{
+    admin0pcode: "ALL",
+    first: {
+      soon: [8],
+      pending: [15],
+      today: [10],
+      reporting_due_date: 10
+    },
+    second: {
+      soon: [23],
+      pending: [22],
+      today: [27],
+      reporting_due_date: 27
+    }
+  }
+]
+
+module.exports.BIWEEKLY_REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG = BIWEEKLY_REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG;
