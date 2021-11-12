baf1ae002cbf14d137d5cad9ad653d11f1c675b7
fakhrihawari <blitzfakhri@gmail.com>
2021-11-12 08:13:32 +0700

    add: default recipient on delete project request-2

--- api/controllers/Cluster/ProjectController.js
+++ api/controllers/Cluster/ProjectController.js
@@ -1799,7 +1799,7 @@ var ProjectController = {
         focal_point = req.param('focal_point');
         focal_point_username = req.param('focal_point_username');
         focal_point_email = req.param('focal_point_email');
-        list_emails =[{email:'ngmreporthub@gmail.com', recipient:'Admin'},{email:'nsadaqatzada@immap.org', recipient:'Admin'}]
+    list_emails = [{ email: 'ngmreporthub@gmail.com', recipient: 'Admin' }, { email: 'nsadaqatzada@immap.org', recipient: 'Admin' }, { email: 'rwitarayoga@immap.org', recipient: 'Admin' }, { email: 'farifin@immap.org', recipient: 'Admin'}]
 
 
     User
